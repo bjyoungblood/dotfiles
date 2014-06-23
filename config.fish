c47988a25f3d5143e74960735a04dc64975b989a
Ben Youngblood <ben@synapsestudios.com>
2014-06-23 14:18:07 -0700

    add fish config

---
+++ config.fish
@@ -0,0 +1,67 @@
+# Fish git prompt
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_showstashstate 'yes'
+set -g __fish_git_prompt_showupstream 'yes'
+
+set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_showupstream "verbose"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "+"
+set -g __fish_git_prompt_char_dirtystate "*"
+set -g __fish_git_prompt_char_untrackedfiles "?"
+set -g __fish_git_prompt_char_conflictedstate "@"
+set -g __fish_git_prompt_char_cleanstate "-"
+set -g __fish_git_prompt_char_stashstate "\$"
+
+set -g __fish_git_prompt_color_upstream
+set -g __fish_git_prompt_color_dirtystate ff5f00
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green bold
+set -g __fish_git_prompt_color_upstream 00afff bold
+
+function fish_prompt --description 'Write out the prompt'
+
+  set -l last_status $status
+
+  if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
+  end
+
+  # PWD
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+  
+  printf '%s ' (__fish_git_prompt)
+  
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
+  
+  echo -n '$ '
+  
+end
+
+function sc
+  /opt/GtkGrab/screenshot
+end
+
+function get_composer
+  wget -O installer.php https://getcomposer.org/installer; and php installer.php; and rm installer.php
+end
+
+set PATH /usr/local/sbin ~/bin $PATH
+set DOCKER_HOST tcp://localhost:4243
+
+function docker
+  /usr/local/bin/docker -H tcp://127.0.0.1:4243 $argv
+end
